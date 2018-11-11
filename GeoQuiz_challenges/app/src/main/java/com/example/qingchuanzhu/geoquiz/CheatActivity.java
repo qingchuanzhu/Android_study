534e924553d0358ef4dbb1b7bcba42d4fd583ea2
Qingchuan Zhu <zqc@utexas.edu>
2018-11-10 21:12:21 -0800

    Added method to send back result to parent activity

--- GeoQuiz_challenges/app/src/main/java/com/example/qingchuanzhu/geoquiz/CheatActivity.java
+++ GeoQuiz_challenges/app/src/main/java/com/example/qingchuanzhu/geoquiz/CheatActivity.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 public class CheatActivity extends AppCompatActivity {
 
     private static final String EXTRA_ANSWER_IS_TRUE = "com.example.qingchuanzhu.geoquiz.answer_is_true";
+    private static final String EXTRA_ANSWER_SHOWN = "com.example.qingchuanzhu.geoquiz.answer_shown";
     private boolean mAnswerIsTrue;
     private Button mShowAnswerButton;
     private TextView mAnswerTextView;
@@ -22,12 +23,26 @@ public class CheatActivity extends AppCompatActivity {
         mAnswerTextView = findViewById(R.id.answer_text_view);
         mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
 
-        mShowAnswerButton.setOnClickListener(view -> mAnswerTextView.setText(mAnswerIsTrue ? R.string.true_button : R.string.false_button));
+        mShowAnswerButton.setOnClickListener(view -> {
+                mAnswerTextView.setText(mAnswerIsTrue ? R.string.true_button : R.string.false_button);
+                setAnswerShowResult(true);
+            }
+        );
     }
 
     public static Intent newIntent(Context packageContext, boolean answerIsTrue) {
         Intent intent = new Intent(packageContext, CheatActivity.class);
         intent.putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue);
         return intent;
+    }
+
+    public static boolean wasAnswerShown(Intent intent) {
+        return intent.getBooleanExtra(EXTRA_ANSWER_SHOWN,false );
+    }
+
+    private void setAnswerShowResult(boolean isAnswerShown) {
+        Intent intent = new Intent();
+        intent.putExtra(EXTRA_ANSWER_SHOWN,isAnswerShown );
+        setResult(RESULT_OK,intent );
     }
 }
--- GeoQuiz_challenges/app/src/main/java/com/example/qingchuanzhu/geoquiz/QuizActivity.java
+++ GeoQuiz_challenges/app/src/main/java/com/example/qingchuanzhu/geoquiz/QuizActivity.java
@@ -13,6 +13,7 @@ public class QuizActivity extends AppCompatActivity {
 
     private static final String TAG = "QuizActivity";
     private static final String KEY_INDEX = "index";
+    private static final int REQUEST_CODE_CHEAT = 0;
 
     private Button mTrueButton;
     private Button mFalseButton;
@@ -60,7 +61,7 @@ public class QuizActivity extends AppCompatActivity {
 
         mCheatButton.setOnClickListener(v -> {
             Intent intent = CheatActivity.newIntent(QuizActivity.this, mQuestionBank[mCurrentIndex].isAnswerTrue());
-            startActivity(intent);
+            startActivityForResult(intent, REQUEST_CODE_CHEAT);
         });
 
         updateQuestion(true);
