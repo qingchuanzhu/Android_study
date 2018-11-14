0bf797bc15657ba921adec668f7815a5c72694f0
Qingchuan Zhu <zqc@utexas.edu>
2018-11-13 16:06:06 -0800

    Created the list row item xml and adapter, view holder inner classes

--- CriminalIntent/app/src/main/java/com/example/qingchuanzhu/criminalintent/CrimeListFragment.java
+++ CriminalIntent/app/src/main/java/com/example/qingchuanzhu/criminalintent/CrimeListFragment.java
@@ -10,6 +10,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.List;
+
 public class CrimeListFragment extends Fragment {
 
     private RecyclerView mCrimeRecyclerView;
@@ -20,6 +22,47 @@ public class CrimeListFragment extends Fragment {
         mCrimeRecyclerView = view.findViewById(R.id.crime_recycler_view);
         // set layout manager
         mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        updateUI();
         return view;
+    }
+
+    private void updateUI() {
+        CrimeLab crimeLab = CrimeLab.get(getActivity());
+        CrimeAdapter adapter = new CrimeAdapter(crimeLab.getCrimes());
+        mCrimeRecyclerView.setAdapter(adapter);
+    }
+
+    // ViewHolder inner class
+    private static class CrimeHolder extends RecyclerView.ViewHolder {
+        public CrimeHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.list_item_crime, parent,false));
+        }
+    }
+
+    // Adapter inner class
+    private class CrimeAdapter extends RecyclerView.Adapter<CrimeHolder> {
+
+        private List<Crime> mCrimes;
+
+        public CrimeAdapter(List<Crime> crimes) {
+            mCrimes = crimes;
+        }
+
+        @Override
+        public int getItemCount() {
+            return mCrimes.size();
+        }
+
+        @NonNull
+        @Override
+        public CrimeHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+            LayoutInflater inflater = getActivity().getLayoutInflater();
+            return new CrimeHolder(inflater,viewGroup );
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull CrimeHolder crimeHolder, int i) {
+
+        }
     }
 }
