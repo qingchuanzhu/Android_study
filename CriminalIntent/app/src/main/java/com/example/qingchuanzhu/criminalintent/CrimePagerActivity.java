9aafe2d1aac7f3875ca7d8eadc0f01ea8f60bace
Qingchuan Zhu <zqc@utexas.edu>
2018-11-16 16:06:32 -0800

    Created the pager activity and set its adapter

---
+++ CriminalIntent/app/src/main/java/com/example/qingchuanzhu/criminalintent/CrimePagerActivity.java
@@ -0,0 +1,47 @@
+package com.example.qingchuanzhu.criminalintent;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.List;
+
+public class CrimePagerActivity extends AppCompatActivity {
+
+    private ViewPager mViewPager;
+    private List<Crime> mCrimes;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime_pager);
+
+        mViewPager = findViewById(R.id.crime_view_pager);
+        FragmentManager fm = getSupportFragmentManager();
+        mViewPager.setAdapter(new CrimePagerAdapter(fm));
+        mCrimes = CrimeLab.get(this).getCrimes();
+    }
+
+    // PagerAdapter Inner class
+    private class CrimePagerAdapter extends FragmentPagerAdapter {
+
+        public CrimePagerAdapter(FragmentManager fm){
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            Crime crime = mCrimes.get(i);
+            return CrimeFragment.newInstance(crime.getID());
+        }
+
+        @Override
+        public int getCount() {
+            return mCrimes.size();
+        }
+    }
+}
